062c8efa510d7728eb5f1d8bb0364de392173c88
UW Portal SDK <aneville@uwaterloo.ca>
2015-03-21 14:34:03 -0400

    Updated: serverscript.js on 3/21/2015 2:34:03 PM

--- serverscript.js
+++ serverscript.js
@@ -10,6 +10,52 @@ function getData() {
     return queryResult;
 }
 
+// Retreive data from the database for a given user ID
+function getAppointmentsForUserId() {
+  //Todo: hide bookings in the past
+    var userId = args.Get("userId");
+  var state = args.Get("status");
+    var queryResult = db.Execute('SELECT * FROM Appointments WHERE studentId = ' + userId + ' AND status = ' + state);
+    var rows = JSON.parse(queryResult);
+    if (rows.length > 0 && typeof rows[0].Error != 'undefined') {
+        return '{"error":"No results"}';
+    }
+    return queryResult;
+}
+
+// Retreive data from the database for a given faculty ID
+function getAppointmentsForFacultyId() {
+  var ressourceId = args.Get("ressourceId");
+  var state = args.Get("status");
+    var queryResult = db.Execute('SELECT * FROM Appointments WHERE staffId = ' + userId + ' AND  status = ' + state);
+    var rows = JSON.parse(queryResult);
+    if (rows.length > 0 && typeof rows[0].Error != 'undefined') {
+        return '{"error":"No results"}';
+    }
+    return queryResult;
+}
+
+// Retreive data from the database for a given faculty ID
+function setState() {
+    var Id = args.Get("appointmentId");
+  var state = args.Get("status");
+    var queryResult = db.Execute('UPDATE Appointments SET status = ' + state + ' WHERE appointmentId = '+ Id);
+    var rows = JSON.parse(queryResult);
+    if (rows.length > 0 && typeof rows[0].Error != 'undefined') {
+        return '{"error":"No results"}';
+    }
+    return queryResult;
+}
+// Retreive data from the database for a given faculty ID
+function makeAppointment() {
+    var queryResult = db.Execute("INSERT INTO Appointments VALUES(0, GETDATE(), @currentUser, @studentName, -1, @ressourceName', @staffName, '-1', @comments)");
+    var rows = JSON.parse(queryResult);
+    if (rows.length > 0 && typeof rows[0].Error != 'undefined') {
+        return '{"error":"No results"}';
+    }
+    return queryResult;
+}
+
 // Create talbe
 function createTable() {
     var debug = {};
@@ -18,7 +64,7 @@ function createTable() {
     var row = JSON.parse(queryResult);
 
     if (row.length > 0 && typeof row[0].Error != 'undefined') {
-        db.Execute('CREATE TABLE sampleTable(id INTEGER PRIMARY KEY IDENTITY(1,1), userId nvarchar(50), value nvarchar(50));');
+        db.Execute("CREATE TABLE Appointments ( appointmentId INTEGER PRIMARY KEY IDENTITY(1,1), status INTEGER, date_time DATETIME,studentId VARCHAR (12), studentName VARCHAR (60), ressourceId VARCHAR (25), ressourceName VARCHAR (25), staffName VARCHAR (60), staffId VARCHAR (12), comments text)");
         debug.result = "created table!";
     } else
         debug.result = "table already exists";
